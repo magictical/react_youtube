9efef9a9a07f6e1f80db141c34205044aa578302
IL KWON KIM <magictical@gmail.com>
2024-01-21 14:46:53 +0900

    deleted_none_used_variables2

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -6,11 +6,7 @@ export default function ChannelInfo({ id, channelTitle }) {
   const { youtube } = useYoutubeApi();
   // const replacedImg =
   //   'https://yt3.ggpht.com/RL_eRSTZjEdc3vAB78wiK6xihSd3wInZ4g1TA3JLzOvxRdJg_2IKqIw8y3CAd03qvgsBb-P5jQ=s48-c-k-c0x00ffffff-no-rj';
-  const {
-    isLoading,
-    error,
-    data: url,
-  } = useQuery({
+  const { data: url } = useQuery({
     queryKey: ['channel', id],
     queryFn: async () => youtube.ChannelData(id),
   });
